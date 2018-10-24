f0d12888d185617df952a0b08413a7d3dec4f677
G <igauravsehrawat@gmail.com>
2018-10-24 13:14:27 +0530

    --add: Delete all events.

--- controllers/events/eraseEvents.js
+++ controllers/events/eraseEvents.js
@@ -1,6 +1,11 @@
+const Event = require('../../models/Event');
 
-const eraseEvents = async () => {
-
+const eraseEvents = async (req, res) => {
+  await Event.destroy({
+    where: {},
+    truncate: true,
+  });
+  return res.status(200).send({});
 };
 
 module.exports = eraseEvents;
