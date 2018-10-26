7e554c5e14bf629b3cf8a3b1178272c1ed2dba87
G <igauravsehrawat@gmail.com>
2018-10-26 17:45:13 +0530

    --hack: onDelete cascade should be implemented, deleting manually other databases.

--- controllers/events/deleteEvents.js
+++ controllers/events/deleteEvents.js
@@ -1,12 +1,29 @@
 const Event = require('../../models/Event');
+const Actor = require('../../models/Actor');
+const Repo = require('../../models/Repo');
 
 const deleteEvents = async (req, res) => {
-  await Event.destroy({
-    where: {},
-    // i think truncate would be fine?
-    // removes the memory allocation
-    truncate: true,
-  });
+  // await Event.destroy({
+  //   where: {},
+  //   // i think truncate would be fine?
+  //   // removes the memory allocation
+  //   truncate: true,
+  // });
+  await Promise.all([
+    Event.destroy({
+      where: {},
+      truncate: true,
+    }),
+    Actor.destroy({
+      where: {},
+      truncate: true,
+    }),
+    Repo.destroy({
+      where: {},
+      truncate: true,
+    }),
+  ]);
+
   return res.status(200).send({});
 };
 
